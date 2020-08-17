49db030dd327568533ce385bfc0094a52b53ff0c
michael thomas <michael.thomas2014@gmail.com>
2020-08-17 01:17:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 
-document.addEventListener("DOMContentLoaded", function(){
-  document.getElementById('text').innerText = "This is really cool!"
+document.addEventListener("DOMContentLoaded", function() {
+  let a = document.querySelector('#text')
+  a.textContent = "This is really cool!"
 });
-
