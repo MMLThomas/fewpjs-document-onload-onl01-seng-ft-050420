9119ada84fa26063bdfba54578d52789637b9697
michael thomas <michael.thomas2014@gmail.com>
2020-08-17 00:55:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
 
 document.addEventListener("DOMContentLoaded", function(){
-  document.getByElementId("text").innerText = "This is really cool!"
+  document.getElementById("text").innerText = "This is really cool!"
 });
